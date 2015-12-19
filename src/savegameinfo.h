67b3effcab746bbd800cd392516605e37c9eb90a
Thomas Tanner <trtanner@btinternet.com>
2015-12-19 20:47:40 +0000

    For TanninOne/modorganizer#418 Changes to support getting list of mods / esps to activate from game plugin

--- src/savegameinfo.h
+++ src/savegameinfo.h
@@ -3,7 +3,10 @@
 
 namespace MOBase { class ISaveGame; }
 
+#include <QMap>
+
 class QString;
+class QStringList;
 
 /** Feature to get hold of stuff to do with save games */
 class SaveGameInfo
@@ -11,9 +14,21 @@ class SaveGameInfo
 public:
   virtual ~SaveGameInfo() {}
 
-  /* Get the information about the supplied save game */
+  /** Get the information about the supplied save game */
   virtual MOBase::ISaveGame const *getSaveGameInfo(QString const &file) const = 0;
 
+  typedef QStringList ProvidingModules;
+  typedef QMap<QString, ProvidingModules> MissingAssets;
+
+  /** Get items missing from save
+   *
+   * @returns a collection of missing assets and the modules that can supply those
+   * assets.
+   *
+   * Note that in the situation where 'module' and 'asset' are indistinguishable,
+   * both still have to be supplied.
+   */
+  virtual MissingAssets getMissingAssets(QString const &file) const = 0;
 };
 
 #endif // SAVEGAMEINFO_H
