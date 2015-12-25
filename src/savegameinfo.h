98a1ccace67f5a9d0ba6a35dce47dc4559bafb33
Thomas Tanner <trtanner@btinternet.com>
2015-12-25 21:49:12 +0000

    Transferring the savegame widget into gamebryo + some cleanup

--- src/savegameinfo.h
+++ src/savegameinfo.h
@@ -2,11 +2,13 @@
 #define SAVEGAMEINFO_H
 
 namespace MOBase { class ISaveGame; }
+namespace MOBase { class ISaveGameInfoWidget; }
 
 #include <QMap>
 
 class QString;
 class QStringList;
+class QWidget;
 
 /** Feature to get hold of stuff to do with save games */
 class SaveGameInfo
@@ -29,6 +31,17 @@ public:
    * both still have to be supplied.
    */
   virtual MissingAssets getMissingAssets(QString const &file) const = 0;
+
+  /** Get a widget to display over the save game list.
+   *
+   * @returns a QT Widget.
+   *
+   * @param parent - parent widget
+   *
+   * It is permitted to return a null pointer to indicate you don't have a
+   * nice visual way of displaying save game contents
+   */
+  virtual MOBase::ISaveGameInfoWidget *getSaveGameWidget(QWidget *parent = 0) const = 0;
 };
 
 #endif // SAVEGAMEINFO_H
