4a4307c4662bbe09bb520d45f4ce6303714d6564
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-09 19:21:46 +0200

    Rollback to a single dataLooksValid() method.

--- src/moddatachecker.h
+++ src/moddatachecker.h
@@ -16,68 +16,6 @@ class ModDataChecker {
 public:
 
   /**
-   * @brief Enumeration indicating the severity level of information.
-   */
-  enum class ModDataSeverity {
-
-    /**
-     * @brief Used for mostly informative message that do not have impact on
-     *     the mod behavior (e.g., a .txt files in an unlikely folder).
-     */
-    INFORMATIVE,
-
-    /**
-     * @brief Used to indicate that something does not look right and might 
-     *     impact the mod behavior.
-     */
-    WARNING,
-
-    /**
-     * @brief Indicates that this mods cannot work if this issue is not fixed.
-     */
-    ERROR
-  };
-
-  /**
-   * @brief Small class encapsulating information about mod data returned by 
-   * methods from MoDataChecker.
-   *
-   */
-  class ModDataInformation {
-
-    /**
-     *
-     */
-    ModDataInformation(ModDataSeverity severity, QString message) : 
-      m_Severity(severity), m_Message(message) { }
-
-    /**
-     * @return the severity of this information.
-     */
-    ModDataSeverity severity() const { return m_Severity; }
-
-    /**
-     * @return the message corresponding to this information.
-     */
-    QString message() const { return m_Message; }
-
-  private:
-
-    ModDataSeverity m_Severity;
-    QString m_Message;
-
-  };
-
-  /**
-   *
-   */
-  enum class CheckResult {
-    VALID,
-    INVALID,
-    UNCERTAIN
-  };
-
-  /**
    * @brief The name of the data folder for the game.
    *
    * For instance, for GameBryo games it is simply "data'.
@@ -89,39 +27,19 @@ public:
   /**
    * @brief Check that the given filetree representing a valid mod layout.
    *
-   * This methods is mainly used during installation to find which installer should
-   * be used or to recurse into multi-level archives, and to quickly indicates to a 
-   * user if a mod looks valid. Heavy operations should be avoided in this method.
+   * This method is mainly used during installation (to find which installer should
+   * be used or to recurse into multi-level archives), or to quickly indicates to a 
+   * user if a mod looks valid.
+   *
+   * This method does not have to be exact, it only has to indicate if the given tree
+   * looks like a valid mod or not by quickly checking the structure (heavy operations
+   * should be avoided).
    *
    * @param tree The tree starting at the root of the "data" folder.
    *
-   * @return whether or not the tree is valid, or if it is not possible
-   *     to tell.
+   * @return whether or not the tree looks valid.
    */
-  virtual CheckResult dataLooksValid(std::shared_ptr<const MOBase::IFileTree> fileTree) const = 0;
-
-  /**
-   * @brief Indicates if this feature can list issues for a mod.
-   *
-   * This method indicates whether `listPotentialIssues` can be used or not.
-   *
-   * @return true if this subfeature is implemented, or false otherwize.
-   */
-  virtual bool canListPotentialIssues() const = 0;
-
-  /**
-   * @brief List potential issues with the given mod layout.
-   *
-   * If this method cannot be used (`canListPotentialIssues()` returns `false`), then an
-   * empty vector should be returned.
-   *
-   * @param tree The tree starting at the root of the "data" folder.
-   *
-   * @returns a list of information about the mod data.
-   *
-   * @see canListPotentialIssues 
-   */
-  virtual std::vector<ModDataInformation> listPotentialIssues(std::shared_ptr<const MOBase::IFileTree> fileTree) const = 0;
+  virtual bool dataLooksValid(std::shared_ptr<const MOBase::IFileTree> fileTree) const = 0;
 
 };
 
