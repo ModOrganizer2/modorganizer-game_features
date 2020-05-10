e713710a789086ae1093d56191c59f31f20c9dbd
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-10 12:37:53 +0200

    Remove usage of ISaveGameInfoWidget.

--- src/savegameinfo.h
+++ src/savegameinfo.h
@@ -2,7 +2,6 @@
 #define SAVEGAMEINFO_H
 
 namespace MOBase { class ISaveGame; }
-namespace MOBase { class ISaveGameInfoWidget; }
 
 #include <QMap>
 
@@ -10,21 +9,26 @@ class QString;
 class QStringList;
 class QWidget;
 
-/** Feature to get hold of stuff to do with save games */
+/**
+ * @brief Feature to get hold of stuff to do with save games.
+ */
 class SaveGameInfo
 {
 public:
   virtual ~SaveGameInfo() {}
 
-  /** Get the information about the supplied save game */
+  using ProvidingModules = QStringList;
+  using MissingAssets = QMap<QString, ProvidingModules>;
+
+  /** 
+   * @brief Get the information about the supplied save game.
+   */
   virtual MOBase::ISaveGame const *getSaveGameInfo(QString const &file) const = 0;
 
-  typedef QStringList ProvidingModules;
-  typedef QMap<QString, ProvidingModules> MissingAssets;
-
-  /** Get items missing from save
+  /** 
+   * @brief Get items missing from save
    *
-   * @returns a collection of missing assets and the modules that can supply those
+   * @return a collection of missing assets and the modules that can supply those
    * assets.
    *
    * Note that in the situation where 'module' and 'asset' are indistinguishable,
@@ -32,19 +36,31 @@ public:
    */
   virtual MissingAssets getMissingAssets(QString const &file) const = 0;
 
-  /** Get a widget to display over the save game list.
-   *
-   * @returns a QT Widget.
-   *
-   * @param parent - parent widget
-   *
-   * It is permitted to return a null pointer to indicate you don't have a
-   * nice visual way of displaying save game contents
+  /**
+   * @return whether or not the save has a paired script extender save.
    */
-  virtual MOBase::ISaveGameInfoWidget *getSaveGameWidget(QWidget *parent = 0) const = 0;
-
-  /** Return whether or not the save has a paired script extender save */
   virtual bool hasScriptExtenderSave(QString const &file) const = 0;
+
+  /**
+   * Get a widget to display over the save game list.
+   *
+   * @param parent The parent widget.
+   *
+   * @return a QT Widget.
+   *
+   * @note It is permitted to return a null pointer to indicate you don't have a
+   * nice visual way of displaying save game contents.
+   */
+  virtual QWidget* getSaveGameWidget(QWidget* parent = 0) const = 0;
+
+  /**
+   * @brief Update the save game widget with the corresponding save.
+   *
+   * @param widget The save game widget initially returned by `getSaveGameWidget()`.
+   * @param save The save game to display in the widget.
+   *
+   */
+  virtual void updateSaveGameWidget(QWidget* widget, QString const& file) const = 0;
 };
 
 #endif // SAVEGAMEINFO_H
